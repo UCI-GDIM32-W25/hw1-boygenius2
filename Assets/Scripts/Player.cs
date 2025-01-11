bdace3209530e6ad6bd338593b1b25f87c9b2528
boygenius2 <ivang85601@gmail.com>
2025-01-10 16:02:15 -0800

    Filled in the scripts with comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,37 @@ public class Player : MonoBehaviour
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
+    // Upon starting the game, set _numSeedsLeft to _numSeeds and _numSeedsPlanted to 0
     private void Start ()
     {
-        
+        _numSeedsPlanted = 0;
+        _numSeedsLeft = _numSeeds;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        // Handling player movement
+        if (Input.GetKey(KeyCode.W) )
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S) )
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A) )
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D) )
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+        // Handling seed planting, if the player has at least one seed
+        if (Input.GetKeyDown(KeyCode.Space) && _numSeedsLeft > 0)
+            PlantSeed();
     }
 
+    // Adds one planted seed to the screen and updates the UI accordingly
     public void PlantSeed ()
     {
-        
+        _numSeedsLeft--;
+        Instantiate(_plantPrefab, _playerTransform.position, new Quaternion() );  // Plants the seed at the player's position
+        _numSeedsPlanted++;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
